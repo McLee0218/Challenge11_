fde576e3d73ff525d97d86b4e33196955a991e59
tamekamclean <tamekasmclean@gmail.com>
2022-10-23 19:27:09 -0400

    work

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -5,11 +5,11 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-router.use('/api', apiRoutes);
+router.use('/html', htmlRoute);
+router.use('/api', apiRoute);
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
 
-
-module.exports= express; 
+module.exports = express; 
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const apiRoutes = require('./routes/apiRoute');
 const app = express();
 const htmlRoutes = require('./routes/htmlRoute');
 const PORT = process.env.PORT || 4444;
-const path = require('path');
+
+// const path = require('path');
 
 
 app.use(express.static("public"));
@@ -15,7 +16,7 @@ app.use("/", htmlRoutes);
 app.use("/", apiRoutes);
 
 
-app.get('@', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 })
 
